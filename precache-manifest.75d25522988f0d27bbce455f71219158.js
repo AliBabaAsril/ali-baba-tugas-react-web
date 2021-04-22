a937d5e7dd9f8f5c3c6b4b9e1e5e4b472e805a7a
AliBabaAsril <61651407+AliBabaAsril@users.noreply.github.com>
2021-04-22 17:33:55 +0800

    Updates

---
+++ precache-manifest.75d25522988f0d27bbce455f71219158.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a87404992e222f3b2e0c6dcb133af02c",
+    "url": "/ali-baba-tugas-react-web/index.html"
+  },
+  {
+    "revision": "c650aff854f9aa75405d",
+    "url": "/ali-baba-tugas-react-web/static/css/2.b7c94619.chunk.css"
+  },
+  {
+    "revision": "a80f8119bac6b68c5614",
+    "url": "/ali-baba-tugas-react-web/static/css/main.faf5950c.chunk.css"
+  },
+  {
+    "revision": "c650aff854f9aa75405d",
+    "url": "/ali-baba-tugas-react-web/static/js/2.79a780ed.chunk.js"
+  },
+  {
+    "revision": "6397acf4c5cafcd591a770ce33fc3e2f",
+    "url": "/ali-baba-tugas-react-web/static/js/2.79a780ed.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a80f8119bac6b68c5614",
+    "url": "/ali-baba-tugas-react-web/static/js/main.fe28763d.chunk.js"
+  },
+  {
+    "revision": "b732d227912cc9b97b8a",
+    "url": "/ali-baba-tugas-react-web/static/js/runtime-main.1f266d16.js"
+  },
+  {
+    "revision": "448c34a56d699c29117adc64c43affeb",
+    "url": "/ali-baba-tugas-react-web/static/media/glyphicons-halflings-regular.448c34a5.woff2"
+  },
+  {
+    "revision": "89889688147bd7575d6327160d64e760",
+    "url": "/ali-baba-tugas-react-web/static/media/glyphicons-halflings-regular.89889688.svg"
+  },
+  {
+    "revision": "e18bbf611f2a2e43afc071aa2f4e1512",
+    "url": "/ali-baba-tugas-react-web/static/media/glyphicons-halflings-regular.e18bbf61.ttf"
+  },
+  {
+    "revision": "f4769f9bdb7466be65088239c12046d1",
+    "url": "/ali-baba-tugas-react-web/static/media/glyphicons-halflings-regular.f4769f9b.eot"
+  },
+  {
+    "revision": "fa2772327f55d8198301fdb8bcfc8158",
+    "url": "/ali-baba-tugas-react-web/static/media/glyphicons-halflings-regular.fa277232.woff"
+  }
+]);
